8e0fc34860b8778094e4a70f4a826f00a0e9928e
shourya27 <thakurshouryapratap@gmail.com>
2022-12-06 18:35:57 +0530

    responsive navbar in react

---
+++ 6-responsive-navbar-react/src/components/Navbar/index.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react';
+import {FaBars,FaTimes} from 'react-icons/fa';
+import logo from '../../assets/img/logo.png';
+import './index.css';
+
+const Navbar = () => {
+    const [click,setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+  return (
+    <>
+    <div className="header">
+        <nav className='navbar'>
+            <img src={logo} alt="logo" />
+            <ul className={click ? 'nav-menu active':'nav-menu'}>
+                <li className='nav-item'><a href="/">Home</a></li>
+                <li className='nav-item'><a href="/">About</a></li>
+                <li className='nav-item'><a href="/">Contact</a></li>
+                <li className='nav-item'><a href="/">Demo</a></li>
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+                {click ? <FaTimes size={30} style={{color:'#fff'}} /> : <FaBars size={30} style={{color:'#fff'}} />}
+            </div>
+        </nav>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
